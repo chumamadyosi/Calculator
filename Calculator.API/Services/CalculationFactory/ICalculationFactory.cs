66b16387a046a972f7be09aaa647e507199bb145
Chuma <chumaM@digikonect.co.za>
2026-02-22 20:17:32 +0200

    Add factorry

---
+++ Calculator.API/Services/CalculationFactory/ICalculationFactory.cs
@@ -0,0 +1,10 @@
+﻿using Calculator.API.Enums;
+using Calculator.API.Services.CalculationStrategy;
+
+namespace Calculator.API.Services.CalculationFactory
+{
+    public interface ICalculationFactory
+    {
+        ICalculationOperation Create(OperationType operationType);
+    }
+}
