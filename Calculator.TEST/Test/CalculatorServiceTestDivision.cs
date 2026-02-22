c51aaa9aca494f4c6bcc84666d71f509d8d5b971
Chuma <chumaM@digikonect.co.za>
2026-02-22 23:32:50 +0200

    Finalise tests

---
+++ Calculator.TEST/Test/CalculatorServiceTestDivision.cs
@@ -0,0 +1,85 @@
+﻿using Calculator.API.Enums;
+using Calculator.API.Models;
+using Calculator.API.Services;
+using Calculator.API.Services.CalculationFactory;
+using Calculator.API.Services.CalculationStrategy;
+using Calculator.TEST.Setup;
+using Moq;
+using Xunit;
+
+namespace Calculator.TEST.Test
+{
+    public class CalculatorServiceTestDivision : IClassFixture<CalculatorServiceFixture>
+    {
+        private readonly CalculatorServiceFixture _fixture;
+
+        public CalculatorServiceTestDivision(CalculatorServiceFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void Execute_Division_PositiveNumbers_ReturnsCorrectResult()
+        {
+            var testData = CalculatorTestDataCollection.GetDivisionPositiveNumbers();
+
+            _fixture._mockFactory.Setup(f => f.Create(testData.Operation)).Returns(new DivideOperation());
+
+            var response = _fixture._calculatorService.Execute(testData.Left,testData.Right,testData.Operation);
+
+            Assert.Equal(testData.ExpectedResult, response.Results);
+            Assert.Equal(CalculationError.None, response.Error);
+        }
+
+        [Fact]
+        public void Execute_Division_WithNegative_ReturnsCorrectResult()
+        {
+            var testData = CalculatorTestDataCollection.GetDivisionWithNegative();
+
+            _fixture._mockFactory.Setup(f => f.Create(testData.Operation)).Returns(new DivideOperation());
+
+            var response = _fixture._calculatorService.Execute( testData.Left, testData.Right,testData.Operation );
+
+            Assert.Equal(testData.ExpectedResult, response.Results);
+            Assert.Equal(CalculationError.None, response.Error);
+        }
+
+        [Fact]
+        public void Execute_Division_BothNegative_ReturnsCorrectResult()
+        {
+            var testData = CalculatorTestDataCollection.GetDivisionBothNegative();
+
+            _fixture._mockFactory.Setup(f => f.Create(testData.Operation)).Returns(new DivideOperation());
+
+            var response = _fixture._calculatorService.Execute(testData.Left,testData.Right, testData.Operation);
+
+            Assert.Equal(testData.ExpectedResult, response.Results);
+            Assert.Equal(CalculationError.None, response.Error);
+        }
+
+        [Fact]
+        public void Execute_Division_ZeroNumerator_ReturnsZero()
+        {
+            var testData = CalculatorTestDataCollection.GetDivisionZeroNumerator();
+
+            _fixture._mockFactory.Setup(f => f.Create(testData.Operation)).Returns(new DivideOperation());
+
+            var response = _fixture._calculatorService.Execute(testData.Left, testData.Right,testData.Operation);
+
+            Assert.Equal(testData.ExpectedResult, response.Results); 
+            Assert.Equal(CalculationError.None, response.Error);
+        }
+
+        [Fact]
+        public void Execute_Division_ByZero_ReturnsError()
+        {
+            var testData = CalculatorTestDataCollection.GetDivisionByZero();
+
+            _fixture._mockFactory.Setup(f => f.Create(testData.Operation)).Returns(new DivideOperation());
+
+            var response = _fixture._calculatorService.Execute(testData.Left,testData.Right,testData.Operation);
+
+            Assert.Equal(CalculationError.DivisionByZero, response.Error);
+        }
+    }
+}
---
+++ Calculator.TEST/Test/CalculatorServiceTestMultiplication.cs
@@ -0,0 +1,68 @@
+﻿using Calculator.API.Enums;
+using Calculator.API.Services.CalculationStrategy;
+using Calculator.TEST.Setup;
+
+namespace Calculator.TEST.Test
+{
+    public class CalculatorServiceTestMultiplication : IClassFixture<CalculatorServiceFixture>
+    {
+        private readonly CalculatorServiceFixture _fixture;
+
+        public CalculatorServiceTestMultiplication(CalculatorServiceFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void Execute_Multiplication_PositiveNumbers_ReturnsCorrectResult()
+        {
+            var testData = CalculatorTestDataCollection.GetMultiplicationPositiveNumbers();
+
+            _fixture._mockFactory .Setup(f => f.Create(testData.Operation)).Returns(new MultiplyOperation());
+
+            var response = _fixture._calculatorService.Execute(testData.Left, testData.Right,testData.Operation);
+
+            Assert.Equal(testData.ExpectedResult, response.Results);
+            Assert.Equal(CalculationError.None, response.Error);
+        }
+
+        [Fact]
+        public void Execute_Multiplication_WithZero_ReturnsCorrectResult()
+        {
+            var testData = CalculatorTestDataCollection.GetMultiplicationWithZero();
+
+            _fixture._mockFactory.Setup(f => f.Create(testData.Operation)) .Returns(new MultiplyOperation());
+
+            var response = _fixture._calculatorService.Execute(testData.Left,testData.Right,testData.Operation);
+
+            Assert.Equal(testData.ExpectedResult, response.Results);
+            Assert.Equal(CalculationError.None, response.Error);
+        }
+
+        [Fact]
+        public void Execute_Multiplication_WithNegative_ReturnsCorrectResult()
+        {
+            var testData = CalculatorTestDataCollection.GetMultiplicationWithNegative();
+
+            _fixture._mockFactory.Setup(f => f.Create(testData.Operation)).Returns(new MultiplyOperation());
+
+            var response = _fixture._calculatorService.Execute( testData.Left, testData.Right,testData.Operation);
+
+            Assert.Equal(testData.ExpectedResult, response.Results);
+            Assert.Equal(CalculationError.None, response.Error);
+        }
+
+        [Fact]
+        public void Execute_Multiplication_BothNegative_ReturnsCorrectResult()
+        {
+            var testData = CalculatorTestDataCollection.GetMultiplicationBothNegative();
+
+            _fixture._mockFactory.Setup(f => f.Create(testData.Operation)) .Returns(new MultiplyOperation());
+
+            var response = _fixture._calculatorService.Execute(testData.Left,testData.Right, testData.Operation );
+
+            Assert.Equal(testData.ExpectedResult, response.Results);
+            Assert.Equal(CalculationError.None, response.Error);
+        }
+    }
+}
