6b98cdf34290d070c4ce7c75289070978cdaef22
Chuma <chumaM@digikonect.co.za>
2026-02-22 23:51:22 +0200

    Add Controller with ijected services, update injection fror all created services in the Program.cs

---
+++ Calculator.API/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿using Calculator.API.Models;
+using Calculator.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController(ICalculatorService calculatorService) : ControllerBase
+    {
+        private readonly ICalculatorService _calculatorService = calculatorService;
+
+        [HttpPost("calculate")]
+        public IActionResult Calculate([FromBody] CalculateRequest request)
+        {
+            var result = _calculatorService.Execute(request.Left, request.Right, request.Operation);
+            if (result.Error != Enums.CalculationError.None)
+            {
+                return BadRequest(new { error = result.Error.ToString() });
+            }
+            return Ok(new { result = result.Results });
+        }
+    }
+}
--- Calculator.API/Program.cs
+++ Calculator.API/Program.cs
@@ -1,4 +1,10 @@
 
+using Calculator.API.Services;
+using Calculator.API.Services.CalculationFactory;
+using Calculator.API.Validation;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
 namespace Calculator.API
 {
     public class Program
@@ -9,10 +15,22 @@ namespace Calculator.API
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(
+                    new System.Text.Json.Serialization.JsonStringEnumConverter());
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddScoped<ICalculationFactory, CalculatorFactory>();
+
+            builder.Services.AddScoped<ICalculatorService, CalculatorService>();
+
+            builder.Services.AddFluentValidationAutoValidation();
+            builder.Services.AddValidatorsFromAssemblyContaining<CalculateRequestValidator>();
 
             var app = builder.Build();
 
