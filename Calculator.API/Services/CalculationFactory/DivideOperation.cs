f25efef30b541993d3d9372986d67feae6b33d6a
Chuma <chumaM@digikonect.co.za>
2026-02-22 23:55:30 +0200

    add other files missing from commits

---
+++ Calculator.API/Services/CalculationFactory/DivideOperation.cs
@@ -0,0 +1,13 @@
+﻿namespace Calculator.API.Services.CalculationStrategy
+{
+    public class DivideOperation : ICalculationOperation
+    {
+        public decimal Calculate(decimal left, decimal right)
+        {
+            if (right == 0)
+
+                throw new DivideByZeroException("Cannot divide by zero.");
+            return left / right;
+        }
+    }
+}
