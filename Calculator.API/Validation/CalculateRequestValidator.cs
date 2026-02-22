b95a173a51ca79e899dbc9c84a7e6b80829e4e8f
Chuma <chumaM@digikonect.co.za>
2026-02-22 23:41:57 +0200

    install fluent validation and validate inputs

---
+++ Calculator.API/Validation/CalculateRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using Calculator.API.Models;
+using FluentValidation;
+
+namespace Calculator.API.Validation
+{
+    public class CalculateRequestValidator : AbstractValidator<CalculateRequest>
+    {
+        public CalculateRequestValidator()
+        {
+            RuleFor(x => x.Left).NotNull().WithMessage("Left value is required.");
+            RuleFor(x => x.Right).NotNull().WithMessage("Right value is required.");
+            RuleFor(x => x.Operation).IsInEnum().WithMessage("Invalid operation type.");
+        }
+    }
+}
